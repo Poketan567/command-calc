e0b259ed17d4d0ec605e4faa3b9717a5c2364cff
Poketan567 <poketan567@gmail.com>
2024-11-16 13:54:01 -0500

    Added function to clear the terminal (Line 4)

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("Hello World!")
+import os
+
+# The function that makes the terminal clear.
+def clear_term():
+    if os.name == "nt":
+        os.system("cls")
+
+    else:
+        os.system("clear")
