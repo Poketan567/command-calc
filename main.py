0bb67c4ccf84c475bf4b04d8968d4c20d9d0c264
Poketan567 <poketan567@gmail.com>
2024-11-16 14:39:58 -0500

    Adds Pi and decimal functionality

--- main.py
+++ main.py
@@ -1,24 +1,33 @@
+import math
 
 val1 = input("Value One: ")
-print(int(val1))
+print(val1)
 
 mathSymbol = input("Symbol: ")
-print(int(val1), mathSymbol)
+print(val1, mathSymbol)
 
 val2 = input("Value Two: ")
 
+# Checks if any of the values have Pi in them.
+if val1 == "pi":
+    val1 = math.pi
+
+elif val2 == "pi":
+    val2 = math.pi
+
+# Checks what symbol was inputted and calculates the answer.
 if mathSymbol == "+":
-    answer = int(val1) + int(val2)
-    print(int(val1), mathSymbol, int(val2), "=", answer)
+    answer = float(val1) + float(val2)
+    print(float(val1), mathSymbol, float(val2), "=", answer)
 
 elif mathSymbol == "-":
-    answer = int(val1) - int(val2)
-    print(int(val1), mathSymbol, int(val2), "=", answer)
+    answer = float(val1) - float(val2)
+    print(float(val1), mathSymbol, float(val2), "=", answer)
 
 elif mathSymbol == "*":
-    answer = int(val1) * int(val2)
-    print(int(val1), mathSymbol, int(val2), "=", answer)
+    answer = float(val1) * float(val2)
+    print(float(val1), mathSymbol, float(val2), "=", answer)
 
 elif mathSymbol == "/":
-    answer = int(val1) / int(val2)
-    print(int(val1), mathSymbol, int(val2), "=", answer)
+    answer = float(val1) / float(val2)
+    print(float(val1), mathSymbol, float(val2), "=", answer)
