137a57dc79cbf8d8bd113421546fc84324f09ada
Poketan567 <poketan567@gmail.com>
2024-11-17 10:17:01 -0500

    Adds the symbol commands "plus", "minus", "times", "divide", and "divided".

--- main.py
+++ main.py
@@ -4,6 +4,20 @@ val1 = input("Value One: ")
 print(val1)
 
 mathSymbol = input("Symbol: ")
+
+# Checks if the math symbol is a word
+if str.lower(mathSymbol) == "plus":
+    mathSymbol = "+"
+
+elif str.lower(mathSymbol) == "minus":
+    mathSymbol = "-"
+
+elif str.lower(mathSymbol) == "times":
+    mathSymbol = "*"
+
+elif str.lower(mathSymbol) == "divide" or "divided":
+    mathSymbol = "/"
+
 print(val1, mathSymbol)
 
 val2 = input("Value Two: ")
@@ -16,18 +30,18 @@ elif val2 == "pi":
     val2 = math.pi
 
 # Checks what symbol was inputted and calculates the answer.
-if mathSymbol == "+":
+if str.lower(mathSymbol) == "+":
     answer = float(val1) + float(val2)
     print(float(val1), mathSymbol, float(val2), "=", answer)
 
-elif mathSymbol == "-":
+elif str.lower(mathSymbol) == "-":
     answer = float(val1) - float(val2)
     print(float(val1), mathSymbol, float(val2), "=", answer)
 
-elif mathSymbol == "*":
+elif str.lower(mathSymbol) == "*":
     answer = float(val1) * float(val2)
     print(float(val1), mathSymbol, float(val2), "=", answer)
 
-elif mathSymbol == "/":
+elif str.lower(mathSymbol) == "/":
     answer = float(val1) / float(val2)
     print(float(val1), mathSymbol, float(val2), "=", answer)
